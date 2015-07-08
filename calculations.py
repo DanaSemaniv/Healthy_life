630ed5f7d6ae733ccf05b90ad2e889a21a3b7f1e
dana <danasemaniv@gmail.com>
2015-07-08 14:05:34 +0300

    calculations for ideal weight and bmi

--- calculations.py
+++ calculations.py
@@ -3,8 +3,10 @@ def getIdealWeightLorentz(height):
     :param height: зріст
     :return: ідеальна вага для заданого зросту
     """
+    height = isHeightInСentimetres(height)
     weight = (height - 100) - (height - 150)/2.0
     return weight
+
 
 def getBMI(height, weight):
     '''
@@ -12,6 +14,16 @@ def getBMI(height, weight):
     :param weight: вага
     :return: індекс маси тіла
     '''
-    height_in_meters = height / 100
+    height_in_meters = isHeightInСentimetres(height) / 100.0
     bmi = weight / height_in_meters ** 2
     return bmi
+
+
+def isHeightInСentimetres(height):
+    '''
+    :param height: зріст
+    :return: зріст в сантиметрах
+    '''
+    if height < 10:
+        return height * 100
+    return height
