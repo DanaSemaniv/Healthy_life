0d9f2565d2b0c637464977a3461e11ff51574325
TarasRudnyk <TarasRudnyk@gmail.com>
2015-07-14 17:06:04 +0300

    Add registration and authorization. Now you can exit from app just clicked "Escape" button

---
+++ authorithation.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+def button_authorization_clicked():
+    print(u"authorization started!")
+    top_authorization = Toplevel()
+    top_authorization.title("authorization")
+    top_authorization.mainloop()
+
